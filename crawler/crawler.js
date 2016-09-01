f66d024546dd1977d7c3f2bea5019a2d293fc929
Francis Bongiovanni <francisjbongiovanni@github.com>
2016-09-01 13:29:50 -0400

    variable names updated

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -3,7 +3,7 @@ var logStream = fs.createWriteStream('./crawler/errorlog.txt', {'flags': 'a'})
 var dateTime = require('node-datetime')
 var dt = dateTime.create().format('m/d/Y H:M:S')
 var jsonfile = require('jsonfile')
-var data = require('./crawl.json')
+var crawlData = require('./crawl.json')
 var chalk = require('chalk')
 var async = require ('async')
 var _ = require('lodash')
@@ -11,126 +11,126 @@ var Nightmare = require('nightmare');
 require('nightmare-iframe-manager')(Nightmare)
 var nightmare = Nightmare({ show: true }) // true displays popup electron window showing results must remove line #17
 
-function crawl(data, cb) {
+function crawl(crawlData, cb) {
   // Visual output
-  console.log(chalk.blue("Starting: " + data.filename))
-  var currentData = require('../public/data/companies/' + data.filename + '.json')
+  console.log(chalk.blue("Starting: " + crawlData.filename))
+  var currentData = require('../public/data/companies/' + crawlData.filename + '.json')
   var nightmare = new Nightmare()
-  if ("iFrame" in data === true) {
-    nightmare.enterIFrame(data.iFrame)
+  if ("iFrame" in crawlData === true) {
+    nightmare.enterIFrame(crawlData.iFrame)
   }
   nightmare
-    .goto(data.url) // go to JSON specified url
-    .wait(data.query) // wait until CSS selector loads
-    .evaluate(function (data, dt) {
+    .goto(crawlData.url) // go to JSON specified url
+    .wait(crawlData.query) // wait until CSS selector loads
+    .evaluate(function (crawlData, dt) {
       employmentType = ["INTERN", "PART-TIME", "PART TIME", "CONTRACT"]
-      arr = []
+      newPositions = []
       // returns text in queried CSS selector
-      query = document.querySelectorAll(data.query) // Job title query
-      links = document.querySelectorAll(data.link)  // Link query
-      locations = document.querySelectorAll(data.locations) // Location query
-      remote = document.querySelectorAll(data.remote) // Location query
+      query = document.querySelectorAll(crawlData.query) // Job title query
+      links = document.querySelectorAll(crawlData.link)  // Link query
+      locations = document.querySelectorAll(crawlData.locations) // Location query
+      remote = document.querySelectorAll(crawlData.remote) // Location query
       /* Set query and link equal to all elements with selector
       itearte through appending text (innerText) from each element
       with job url to obj*/
       // If last element(s) with same query are not jobs
-      if ("amtExtraElements" in data === false) {
+      if ("amtExtraElements" in crawlData === false) {
 	var x = 0
       } else {
-	var x = data.amtExtraElements
+	var x = crawlData.amtExtraElements
       }
       var i
       for (i = 0; i < query.length - x; i++) {
-	var obj = new Object()
-	obj["@context"] = "http://schema.org"
-	obj["@type"] = "JobPosting"
+	var position = new Object()
+	position["@context"] = "http://schema.org"
+	position["@type"] = "JobPosting"
 	// if multiple locations need parsing parseLocation may be array
-	if (data.parseLocation instanceof Array) {
-	  z = data.parseLocation.length
+	if (crawlData.parseLocation instanceof Array) {
+	  z = crawlData.parseLocation.length
 	} else {
 	  z = 1
 	}
 	for (y = 0; y < z; y++) {
 	// if location is static or locations attribute contains correct location (e.g string locations contains "Philadelphia" not "San Francisco")
-	  if ("locations" in data === false || locations[i].innerText.trim() !== undefined && locations[i].innerText.trim().includes(data.parseLocation[y])){
-	    obj.title =  query[i].innerText.trim()
-	  // Add link for each title unless data.link is null in which case make same as data.url
-	  if (data.link !== null) {
-	    obj.url = links[i].href
+	  if ("locations" in crawlData === false || locations[i].innerText.trim() !== undefined && locations[i].innerText.trim().includes(crawlData.parseLocation[y])){
+	    position.title =  query[i].innerText.trim()
+	  // Add link for each title unless crawlData.link is null in which case make same as crawlData.url
+	  if (crawlData.link !== null) {
+	    position.url = links[i].href
 	  } else {
-	    obj.url = data.url
+	    position.url = crawlData.url
 	  }
 	  // If jobLocation is static (created in input JSON) else add location based on HTML element that contains location
-	  if ("jobLocation" in data === true) {
-	    obj.jobLocation = data.jobLocation
+	  if ("jobLocation" in crawlData === true) {
+	    position.jobLocation = crawlData.jobLocation
 	  } else {
-	    obj.jobLocation = locations[i].innerText.trim()
+	    position.jobLocation = locations[i].innerText.trim()
 	  }
-	  obj.employmentType = "full-time"
+	  position.employmentType = "full-time"
 	  for (k = 0; k < employmentType.length; k++) {
 	    if (query[i].innerText.toUpperCase().trim().includes(employmentType[k])) {
-	      obj.employmentType = employmentType[k].toLowerCase()
+	      position.employmentType = employmentType[k].toLowerCase()
 	    }
 	  }
 	  // Default remote to false, true if if JSON val is true, if string includes parser true otherwise false
-	  if ("remote" in data === false) {
-	      obj.remote = false
-	  } else if (data.remote === true || remote[i].innerText.trim().includes(data.parseRemote)) {
-	      obj.remote = true
+	  if ("remote" in crawlData === false) {
+	      position.remote = false
+	  } else if (crawlData.remote === true || remote[i].innerText.trim().includes(crawlData.parseRemote)) {
+	      position.remote = true
 	  } else {
-	      obj.remote = false
+	      position.remote = false
 	  }
 	  // If sliceTitle is array each item oterwise slice integer
-	  if ("sliceTitle" in data === true) {
-	    if (data.sliceTitle instanceof Array === true) {
-	      obj.title = obj.title.slice(data.sliceTitle[0], data.sliceTitle[1])
+	  if ("sliceTitle" in crawlData === true) {
+	    if (crawlData.sliceTitle instanceof Array === true) {
+	      position.title = position.title.slice(crawlData.sliceTitle[0], crawlData.sliceTitle[1])
 	    } else {
-	      obj.title = obj.title.slice(data.sliceTitle)
+	      position.title = position.title.slice(crawlData.sliceTitle)
 	    }
 	  }
-	  arr.push(obj)
+	  newPositions.push(position)
 	}
 	var dt = new Date()
 	var dt = dt.getFullYear() + '-' + ("0" + dt.getMonth() ).slice(-2) + '-' + ("0" + dt.getDate()).slice(-2)
-	obj.datePosted = dt
+	position.datePosted = dt
       }
     }
-      return arr
-    }, data)
+      return newPositions
+    }, crawlData)
   .end()
-  .then(function (arr) {
+  .then(function (newPositions) {
     // quick fix for null items (coming from parsing locations with 2 matching values from parseLocation array)
-    arr = arr.filter(function(e){return e})
-    // If position already in data leave, update new positions and remove old
-    console.log(arr)
-    x = _.unionBy(currentData.positions, arr, 'title')
-    currentData.positions = _.intersectionBy(x, arr, 'title')
+    newPositions = newPositions.filter(function(e){return e})
+    // If position already in crawlData leave, update new positions and remove old
+    console.log(newPositions)
+    x = _.unionBy(currentData.positions, newPositions, 'title')
+    currentData.positions = _.intersectionBy(x, newPositions, 'title')
     // Write to each company to json file
-    var file = './public/data/companies/' + data.filename + '.json'
+    var file = './public/data/companies/' + crawlData.filename + '.json'
     jsonfile.writeFile(file, currentData, {spaces: 2}, function(err) {
       if (err) {
-	console.error(err)
+        console.error(err)
       }
     })
-    console.log(chalk.green('Finished: ' + data.filename))
+    console.log(chalk.green('Finished: ' + crawlData.filename))
     cb()
   })
   .catch(function (error) {
     currentData.positions = []
-    var file = './public/data/companies/' + data.filename + '.json'
+    var file = './public/data/companies/' + crawlData.filename + '.json'
     jsonfile.writeFile(file, currentData, {spaces: 2}, function(err) {
       if (err) {
-	console.error(err)
+        console.error(err)
       }
     })
     // Write error to log and print to console
-    logStream.write('-----------------------------------------------' + '\n' + data.filename + '\n' + error + '\n' + dt + '\n')
-    console.log(chalk.red('Error: ' + data.filename + '\n' + error))
+    logStream.write('-----------------------------------------------' + '\n' + crawlData.filename + '\n' + error + '\n' + dt + '\n')
+    console.log(chalk.red('Error: ' + crawlData.filename + '\n' + error))
     cb()
   })
 }
 
 
-async.eachSeries(data, crawl, function (err){
+async.eachSeries(crawlData, crawl, function (err){
     console.log(chalk.magenta('done!'))
 })
